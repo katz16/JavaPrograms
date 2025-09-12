cba0bcf990fc74aa3ed7b91db77d86351f236b9e
katz16 <karthikkumar654@gmail.com>
2025-09-12 18:01:49 +0530

    Singleton design pattern

---
+++ src/main/java/org/example/designpatterns/Singleton.java
@@ -0,0 +1,23 @@
+package org.example.designpatterns;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println("Are both instances the same? " + (singleton1 == singleton2));
+    }
+}
